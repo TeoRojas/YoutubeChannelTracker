7e6786cbc15a9c0c1b38193c0ef62cd87ee864ec
TeoRojas <teofilo.rojas.mata@gmail.com>
2020-11-17 20:46:16 +0100

    pep0008 style

--- YoutubeChannelsTracker.py
+++ YoutubeChannelsTracker.py
@@ -2,43 +2,61 @@
 
 from myFunctions import *
 
-cls()
-channels = []
-channelsDriver = []
-minSubs = 19
-minVids = 5
-SCROLL_PAUSE_TIME = 3
-fields = ['Channel Name', 'Subscribers', 'Videos', 'Channel Link']
-filename = "youtubeChannels.csv"
-orderBy = {'relevance':'CAASAhAC', 'uploadDate':'CAISAhAC', 'numberOfDisplays':'CAMSAhAC', 'score':'CAESAhAC'}
-channelTypeSearched = 'tecnologia'
+reset_screen()
+SCROLL_PAUSE_TIME = 1
+MIN_SUBS = 19
+MIN_VIDS = 5    
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(10)
-url = createUrl(channelTypeSearched, orderBy['uploadDate'])
-driver.get(url)
+def main():
+    channels = []
+    driver_format_channels = []
+    orderBy = {
+        'relevance':'CAASAhAC', 
+        'uploadDate':'CAISAhAC', 
+        'numberOfDisplays':'CAMSAhAC', 
+        'score':'CAESAhAC'
+        }
+    type_of_channel_searched = 'geek'   
+    filename = (
+        type_of_channel_searched 
+        + '_YT_channels.csv'
+        )
 
-agreeCookiesPopUp(driver)
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    url = create_url(type_of_channel_searched, orderBy['relevance'])
+    driver.get(url)
 
-scroll(driver, 5)
+    agree_cookies_pop_up(driver)
 
-channelsDriver = driver.find_elements(By.ID, 'main-link') 
+    scroll(driver, 1)
 
-for channelD in channelsDriver:
-    nSubs, nVids = getNsubsAndNvids(channelD)
+    driver_format_channels = driver.find_elements(By.ID, 'main-link') 
 
-    if nSubs >= minSubs and nVids >= minVids:
-        c = returnChannel(channelD)
-        channels.append(c)
+    for driver_channel in driver_format_channels:
+        number_of_subs = get_number_of_subs(driver_channel) 
+        number_of_vids = get_number_of_vids(driver_channel)
+
+        if number_of_subs >= MIN_SUBS and number_of_vids >= MIN_VIDS:
+            channels.append(returnChannel(driver_channel))
+
+    driver.quit()
 
 
-driver.quit()
+    with open(filename, 'w') as file:
+        fieldnames = [
+            'Channel Name',
+            'Subscribers',
+            'Videos',
+            'Channel Link'
+            ]
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(fieldnames)
+        for c in channels:
+            writer.writerow([c.name, c.number_of_subs, c.number_of_vids, c.link]) 
 
-file = open(filename, 'w')
-file.write("Channel Name;Subscribers;Videos;Channel Link;\n")
-for c in channels:
-    file.write(c.name +";"+ str(c.nSubs) +";"+ str(c.nVids) +";"+ c.link +";"+ "\n")
+    print("Channels found: " + str(len(channels)))
 
-file.close()
 
-print("Channels found: " + str(len(channels)))
+if __name__ == '__main__':
+    main()
--- myFunctions.py
+++ myFunctions.py
@@ -2,19 +2,35 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from urllib.parse import urlparse
 import time
 import os
 import csv
 
 
-def document_initialised(driver):
-    return driver.execute_script("return initialised")
+class Channel:
+    def __init__ (self, name, link, subs, vids):
+        self.name = name
+        self.link = link
+        self.number_of_subs = subs
+        self.number_of_vids = vids
+    
 
-def cls():
+def create_url(type_of_channel_searched, orderBy):
+    url = ('https://www.youtube.com/results?search_query=' 
+            + type_of_channel_searched 
+            + '&sp=CAISAhAC' 
+            + orderBy)
+    url = urlparse(url).geturl()
+    return url
+
+
+def reset_screen():
     os.system("reset")
 
-def createUrl(channelTypeSearched, orderBy):
-    return 'https://www.youtube.com/results?search_query=' + channelTypeSearched +'&sp=CAISAhAC' + orderBy 
+
+def document_initialised(driver):
+    return driver.execute_script("return initialised")
 
 
 def scroll(driver, timeout):
@@ -23,7 +39,8 @@ def scroll(driver, timeout):
     # Get scroll height
     last_height = driver.execute_script("return document.documentElement.scrollHeight")
     count = 1
-    while True:
+    for i in range(1):
+    #while True:
         # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
 
@@ -40,81 +57,65 @@ def scroll(driver, timeout):
         last_height = new_height 
 
 
-def agreeCookiesPopUp(driver):
+def agree_cookies_pop_up(driver):
     driver.switch_to.frame("iframe")
     driver.find_element_by_xpath('//*[@id="introAgreeButton"]/span/span').click()
     driver.switch_to.default_content()  
 
 
-def nVidsInteger(nVidsAux):
-    nVids = 0
-    if (len(nVidsAux) == 7):
-        nVids = int(nVidsAux[:-6])
-    elif(len(nVidsAux) > 7):
-            nVids = int(nVidsAux[:-7].replace('.',''))
-    return nVids
+def number_of_vids_to_integer(number_of_vids_in_string):
+    number_of_vids = 0
+    if (len(number_of_vids_in_string) == 7):
+        number_of_vids = int(number_of_vids_in_string[:-6])
+    elif(len(number_of_vids_in_string) > 7):
+            number_of_vids = int(number_of_vids_in_string[:-7].replace('.',''))
+    return number_of_vids
 
-def nSubsInteger(nSubsAux):
-    nSubs = 0
-    if (len(nSubsAux) == 12):
+def number_of_subs_to_integer(number_of_subs_in_string):
+    number_of_subs = 0
+    if (len(number_of_subs_in_string) == 12):
         # Convert "1 suscriptor" string to "1" integer
-        nSubs = int(nSubsAux[:-11])
-    elif(len(nSubsAux) >= 14):
-        if nSubsAux.find("M") != -1:
-            if nSubsAux.find(",") != -1:
-                commaIndex = nSubsAux.find(",")
-                spaceIndex = nSubsAux.find(" M")
+        number_of_subs = int(number_of_subs_in_string[:-11])
+    elif(len(number_of_subs_in_string) >= 14):
+        if number_of_subs_in_string.find("M") != -1:
+            if number_of_subs_in_string.find(",") != -1:
+                commaIndex = number_of_subs_in_string.find(",")
+                spaceIndex = number_of_subs_in_string.find(" M")
                 zerosToAdd = 6 - ((spaceIndex - 1) - commaIndex)
                 zeros = zerosToAdd*'0'
                 # Convert "1,1 M suscriptores" string to "1100000 suscriptores" string          
-                nSubsAux = nSubsAux.replace(',','')
-                nSubsAux = nSubsAux.replace(' M', zeros)
+                number_of_subs_in_string = number_of_subs_in_string.replace(',','')
+                number_of_subs_in_string = number_of_subs_in_string.replace(' M', zeros)
             else:
                 # Convert "1 M suscriptores" string to "1100000 suscriptores" string          
-                nSubsAux = nSubsAux.replace(' M','000000')  
+                number_of_subs_in_string = number_of_subs_in_string.replace(' M','000000')  
         # Convert "x suscriptores" string to "x" integer             
-        nSubs = int(nSubsAux[:-13].replace('.',''))
-    return nSubs      
+        number_of_subs = int(number_of_subs_in_string[:-13].replace('.',''))
+    return number_of_subs      
 
-class Channel:
-    def __init__ (self, name, link, subs, vids):
-        self.name = name
-        self.link = link
-        self.nSubs = subs
-        self.nVids = vids
+def get_number_of_subs(element):
+    number_of_subs_in_string = element.find_element(By.ID, 'metadata').find_element(By.ID, 'subscribers').text # need to delete the tail 'suscriptores' -13 characters.    
+    number_of_subs = number_of_subs_to_integer(number_of_subs_in_string)
+    return number_of_subs
 
-def getNsubsAndNvids(element):
-    nSubsAux = element.find_element(By.ID, 'metadata').find_element(By.ID, 'subscribers').text # need to delete the tail 'suscriptores' -13 characters.
-    nVidsAux = element.find_element(By.ID, 'metadata').find_element(By.ID, 'video-count').text    
-    
-    nSubs = nSubsInteger(nSubsAux)
-    nVids = nVidsInteger(nVidsAux)
-
-    return nSubs, nVids
-
-def getNsubs(element):
-    nSubsAux = element.find_element(By.ID, 'metadata').find_element(By.ID, 'subscribers').text # need to delete the tail 'suscriptores' -13 characters.    
-    nSubs = nSubsInteger(nSubsAux)
-    return nSubs
-
-def getNvids(element):
-    nVidsAux = element.find_element(By.ID, 'metadata').find_element(By.ID, 'video-count').text       
-    nVids = nVidsInteger(nVidsAux)
-    return nVids
+def get_number_of_vids(element):
+    number_of_vids_in_string = element.find_element(By.ID, 'metadata').find_element(By.ID, 'video-count').text       
+    number_of_vids = number_of_vids_to_integer(number_of_vids_in_string)
+    return number_of_vids
 
 def getLink(element):
     return element.get_attribute('href')
 
 def returnChannel(element): #Returns a formatted Channel
     channel = Channel(element.find_element(By.TAG_NAME , 'ytd-channel-name').text,
-    getLink(element),
-    getNsubs(element),
-    getNvids(element))
+                    getLink(element),
+                    get_number_of_subs(element),
+                    get_number_of_vids(element))
 
     return channel
 
 def printChannels(channels):
     for c in channels:
         print (c.name)
-        print ("Subs: " + str(c.nSubs) + " | " + "Vids: " + str(c.nVids))    
+        print ("Subs: " + str(c.number_of_subs) + " | " + "Vids: " + str(c.number_of_vids))    
         print ("------------")
